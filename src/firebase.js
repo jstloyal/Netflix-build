29e024725fd66c7505d1bf1733286041a69410ee
jstloyal <jstloyalty@gmail.com>
2021-02-10 14:59:06 +0100

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDqEm6d5CgGw_mLBifQr29T14Y9qGTFNkg",
+  authDomain: "netflix-build-d3ed1.firebaseapp.com",
+  projectId: "netflix-build-d3ed1",
+  storageBucket: "netflix-build-d3ed1.appspot.com",
+  messagingSenderId: "348204037091",
+  appId: "1:348204037091:web:7af19707fe5c9ead6b26d6"
+};
