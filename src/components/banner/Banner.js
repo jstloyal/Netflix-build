d680f54b455fe211520c0dbbf253d662c1cd231a
jstloyal <jstloyalty@gmail.com>
2021-02-11 08:09:41 +0100

    install axios and add API key and endpoints

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -1,20 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from '../../API/axios';
+import requests from '../../API/request';
+
 import './Banner.css';
 
 function Banner() {
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchNetflixOriginals);
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length -1)
+        ]
+      );
+      return request;
+    }
+    fetchData();
+  }, []);
+
+  console.log(movie);
+
+  function truncate(string, n) {
+    return string?.length > n ? string.substr(0, n -1) + '...' : string;
+  };
+
   return (
-    <header className="banner" style={{ backgroundImage: `url('https://i.imgur.com/e1hLQ2m.png')`,
+    <header className="banner" style={{ backgroundImage: `url('https://image.tmdb.org/t/p/original/${movie?.backdrop_path}')`,
     backgroundSize: "cover" ,
     backgroundPosition: "center center"}}>
       <div className="banner-content">
         <h1 className="banner-title">
-          Movies Name
+          {movie?.title || movie?.name || movie?.original_name}
         </h1>
         <div className="banner-buttons">
-          <button>Play</button>
-          <button>My List</button>
+          <button className='banner-button'>Play</button>
+          <button className='banner-button'>My List</button>
         </div>
+        <h1 className="banner-description">
+          {truncate(movie?.overview, 150)}
+        </h1>
       </div>
+      <div className="banner-fadeButton" />
     </header>
   )
 }
---
+++ src/components/row/Row.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Row.css';
+
+function Row() {
+  return (
+    <div className='row'>
+      
+    </div>
+  )
+}
+
+export default Row;
