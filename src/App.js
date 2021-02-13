7b9ce6155c6010a4a8eba8865123576ca80eb3aa
jstloyal <jstloyalty@gmail.com>
2021-02-13 07:48:18 +0100

    connect app to the redux store

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
- 
+import { auth } from './firebase';
 import HomeScreen from './pages/home-screen/HomeScreen';
 import LoginPage from './pages/login/LoginPage';
+import { login, logout, selectUser } from './app/features/userSlice';
 
 import './App.css';
-import { auth } from './firebase';
 
 function App() {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+  
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged(userAuth => {
       if (userAuth) {
-        console.log(userAuth);
+        dispatch(login({
+          uid: userAuth.uid,
+          email: userAuth.email,
+        }));
       } else {
-        // logged out 
+        dispatch(logout); 
       }
     });
 
     return unsubscribe;
   }, []);
-
-  const user = null;
 
   return (
     <div className="app">
@@ -29,6 +34,7 @@ function App() {
           <LoginPage />
         ) : (
           <Switch>
+            
             <Route exact path="/">
               <HomeScreen />
             </Route>
